6005f25b428d747d65bf7622dc3aadef3574dfca
Sam Mills <sammills@Esthers-MacBook-Air.local>
2018-03-12 12:20:44 +0000

    created relationship between Users and Posts

--- app/models.py
+++ app/models.py
@@ -5,7 +5,8 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
-  password_hash = db.Column(db.String(120))
+  password_hash = db.Column(db.String(120))]
+  posts db.relationship( 'Post', backref='author', lazy ='dynamic')
 
   def __repr__(self):
     return '<User {}', format(self.username)
